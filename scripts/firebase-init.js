8c47596ee939cfd536a5e3abace295317417e0c5
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2018-07-03 21:18:10 -0700

    Removed API Keys
    I removed the sensitive Firebase config requirements such as the API key and the messaging sender ID.

--- scripts/firebase-init.js
+++ scripts/firebase-init.js
@@ -1,10 +1,10 @@
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyDKTgfM6QXtFhUdtpc40ZUb4etNfuwDPzc",
+  apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
   authDomain: "ethans-stickies.firebaseapp.com",
   databaseURL: "https://ethans-stickies.firebaseio.com",
   projectId: "ethans-stickies",
   storageBucket: "ethans-stickies.appspot.com",
-  messagingSenderId: "714831940211"
+  messagingSenderId: "999999999999"
 });
 
 const db = firebase.firestore(app);
